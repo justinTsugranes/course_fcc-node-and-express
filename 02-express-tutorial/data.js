0c4b200bc407dddd808c2f8824b4d6c14fc715e9
justintsugranes <justintsugranes@proton.me>
2022-12-19 10:31:16 -0500

    setup simple express server

--- 02-express-tutorial/data.js
+++ 02-express-tutorial/data.js
@@ -5,7 +5,7 @@ const products = [
     image:
       'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
     price: 39.95,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim Venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
   },
   {
     id: 2,
