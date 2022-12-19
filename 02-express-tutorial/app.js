2bfc08b7eb6b8a34891e50c1ebb5c50fe933e539
justintsugranes <justintsugranes@proton.me>
2022-12-19 14:31:34 -0500

    add express router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,30 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const logger = require('./logger')
-const authorize = require('./authorize')
 
-// req => middleware => res
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
-// MIDDLEWARE
-// app.use invokes for everything that follows
-// you can specify a base path (ie app.use('/api', logger)) that the middleware will use. otherwise, it will apply to everything
-app.use([logger, authorize])
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
-// ROUTE METHODS
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About Page')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products Page')
-})
-app.get('/api/reviews', (req, res) => {
-  res.send('Reviews Page')
-})
+app.use('/api/people', people)
+app.use('/login', auth)
 
 // SERVER
 app.listen(port, () => {
